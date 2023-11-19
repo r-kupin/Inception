ad04dce32e649d256fc6c95b582f7f919a73b94d
Roman Kupin <rokupin@student.42.fr>
2023-11-19 19:39:26 +0100

    nice and private

---
+++ srcs/requirements/wordpress/tools/make_wp_core_install_script.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+cat >> wp_core_install.sh << EOF
+#!/bin/sh
+wp_admin='${WP_ADMIN}'
+wp_admin_pass='${WP_ADMIN_PASS}'
+wp_admin_mail='${WP_ADMIN_MAIL}'
+
+wp_user='${WP_USER}'
+wp_user_pass='${WP_USER_PASS}'
+wp_user_mail='${WP_USER_MAIL}'
+
+if ! wp core is-installed; then
+    wp core install \
+        --url="https://localhost:42443" \
+        --title="Inception" \
+        --admin_user="\$wp_admin" \
+        --admin_password="\$wp_admin_pass" \
+        --admin_email="\$wp_admin_mail"
+
+    wp user create \
+        "\$wp_user" \
+	"\$wp_user_mail" \
+	--user_pass="\$wp_user_pass"
+fi
+EOF
+
+cat >> entrypoint.sh << EOF
+#!/bin/sh
+
+sh wp_core_install.sh
+rm wp_core_install.sh
+exec "\$@"
+
+EOF
+
+chmod +x wp_core_install.sh entrypoint.sh
